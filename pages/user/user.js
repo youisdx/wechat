9ac3ecdbc317d63259f046f1fd90cf7067a8426b
kaixin1013 <2413890705@qq.com>
2019-03-28 11:10:06 +0800

    new

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,66 +1,72 @@
-// pages/user/user.js
 Page({
 
   /**
-   * 页面的初始数据
-   */
+  * 页面的初始数据
+  */
   data: {
-
+    userInfo: {},
+    sex_array: ['保密', '男', '女']
   },
 
   /**
-   * 生命周期函数--监听页面加载
-   */
+  * 生命周期函数--监听页面加载
+  */
   onLoad: function (options) {
-
+    this.setData({
+      userInfo: wx.getStorageSync('userInfo')
+    })
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  * 修改个人信息
+  */
+  choseImage: function () {
+    //修改头像需用到wx.uploadfile()上传至服务器
+    //这里不开放图片上传的接口，有需要可学习后台相关自行搭建服务器
+    wx.showToast({
+      title: '头像暂不支持修改',
+      icon: 'none'
+    })
+  },
+  bindName: function () {
+    wx.navigateTo({
+      url: './change?changeWhat=name',
+    })
+  },
+  bindTel: function () {
+    wx.navigateTo({
+      url: './change?changeWhat=tel',
+    })
+  },
+  bindSex: function () {
+    wx.navigateTo({
+      url: './change?changeWhat=sex',
+    })
+  },
+  bindSchool: function () {
+    wx.navigateTo({
+      url: './change?changeWhat=school',
+    })
+  },
+  bindNumber: function () {
+    wx.navigateTo({
+      url: './change?changeWhat=number',
+    })
+  },
+  bindYear: function () {
+    wx.navigateTo({
+      url: './change?changeWhat=enter_year',
+    })
   },
 
   /**
-   * 生命周期函数--监听页面显示
-   */
+  * 生命周期函数--监听页面显示
+  */
   onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      userInfo: wx.getStorageSync('userInfo')
+    })
   }
 })
+
+    
