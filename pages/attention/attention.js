fc158f595a9891fd9e84d26d22eee99af2062b98
youisdx <1601764856qq.com>
2019-03-28 09:03:09 +0800

    wechat

---
+++ pages/attention/attention.js
@@ -0,0 +1,77 @@
+// pages/attention/attention.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  // array:[
+  //   {
+  //     avatar:'images/1.jpeg',
+  //     name:'创业',
+  //     word:'世界很美好，世道很艰难'
+
+  //   },
+  //   {
+
+  //   },
+  // ]
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
